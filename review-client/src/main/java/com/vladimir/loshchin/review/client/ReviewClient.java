7cae369c770297084326c98c327bbe17e2bfba73
Vladimir Loshchin <vladimirloshchin@cloud.upwork.com>
2023-05-12 17:24:46 +0600

    LLC-3880: add custom skills adobtion :publish: :deploy-staging:

---
+++ review-client/src/main/java/com/vladimir/loshchin/review/client/ReviewClient.java
@@ -0,0 +1,41 @@
+package com.vladimir.loshchin.review.client;
+
+import com.vladimir.loshchin.review.model.Rating;
+
+import reactor.core.publisher.Mono;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.time.Duration;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.JdkClientHttpConnector;
+
+@Component
+public class ReviewClient {
+
+    private WebClient webClient;
+    
+    public ReviewClient() {
+        HttpClient httpClient = HttpClient.newBuilder()
+            .followRedirects(Redirect.NORMAL)
+            .connectTimeout(Duration.ofSeconds(3))
+            .build();
+
+        ClientHttpConnector connector =
+            new JdkClientHttpConnector(httpClient);
+
+        webClient = WebClient.builder().clientConnector(connector).build();
+    }
+    
+    public Mono<Rating> getRating(String productId) {
+        return webClient.get()
+                .uri("http://localhost:8081/rating/{productId}", productId)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(Rating.class);
+    }
+}
